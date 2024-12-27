f5c29105dddc6209b5c31a50b2e5f1c1133f256a
Renan <renanjuniodossantos13@gmail.com>
2024-12-27 15:58:49 -0300

    .

--- exercícios/aula 11/ex008.js
+++ exercícios/aula 11/ex008.js
@@ -1,1 +1,6 @@
-console.log('Está funcionando corretamente');
+var país = 'EUA'
+console.log(`Seu país de origem é ${país}`)
+if (país == 'Brasil')   
+    console.log(`Você mora no Brasil`)
+else
+    console.log(`Você é estrangeiro`)
